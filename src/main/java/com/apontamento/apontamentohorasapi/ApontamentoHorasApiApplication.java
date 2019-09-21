98824c420f81311ef74fabbff93117579120f4ff
Erick Henrique <erickhenriquemota@gmail.com>
2019-09-20 22:49:54 -0300

    Criação projeto

---
+++ src/main/java/com/apontamento/apontamentohorasapi/ApontamentoHorasApiApplication.java
@@ -0,0 +1,13 @@
+package com.apontamento.apontamentohorasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApontamentoHorasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApontamentoHorasApiApplication.class, args);
+    }
+
+}
